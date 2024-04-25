829e7635430ae1ab98847bb9abe40e7979fe781a
Fahim Mohammod Fardous <fahim.fardous@brainstation-23.com>
2024-04-25 11:59:56 +0600

    Added new code in basic_program file

---
+++ basic_program.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Hello world"<<endl;
+    int num;
+    cin>>num;
+    int sum = 0;
+    for(int i=1;i<=num;i++){
+        sum+=i;
+    }
+    cout<<sum<<endl;
+}
